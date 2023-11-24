ab3f6ff68901e2291fe50e09c3d74bdc56f7ee4a
Prakash Srivastava <pra.sri111@gmail.com>
2023-11-24 22:05:15 +0530

    soft delete implementation for todos table

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Todo extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = ['task_name', 'description', 'is_complete'];
+
+    protected $hidden = ['updated_at', 'deleted_at'];
 }
