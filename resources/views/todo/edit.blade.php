f449b951358881c9b7362542620e53c29328b54e
Prakash Srivastava <pra.sri111@gmail.com>
2023-11-29 18:21:06 +0530

    validation in add/edit form below fields

--- resources/views/todo/edit.blade.php
+++ resources/views/todo/edit.blade.php
@@ -12,11 +12,17 @@
         <div class="mb-3">
             <label for="taskName" class="form-label">Task name *</label>
             <input type="text" class="form-control" id="taskName" placeholder="Enter task name" name="task_name" value="{{ $todo->task_name }}">
+            @error('task_name')
+                <p class="text-danger small">{{ $message }}</p>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="taskDescription" class="form-label">Description</label>
             <textarea class="form-control" id="taskDescription" rows="3" placeholder="Enter task description" name="description">{{ $todo->description }}</textarea>
+            @error('description')
+                <p class="text-danger small">{{ $message }}</p>
+            @enderror
         </div>
 
         <div class="mb-3">
@@ -25,6 +31,9 @@
                 <option value="0" {{ $todo->is_complete == 0 ? 'selected=selected' : '' }}>Not Complete</option>
                 <option value="1" {{ $todo->is_complete == 1 ? 'selected=selected' : '' }}>Complete</option>
             </select>
+            @error('is_complete')
+                <p class="text-danger small">{{ $message }}</p>
+            @enderror
         </div>
 
         <input type="submit" class="btn btn-primary btn-sm" value="Save" />
